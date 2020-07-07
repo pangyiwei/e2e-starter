ae630ebd171dfd09ea7f73c12b8d7050a14a9aae
Yi Wei <pangyiwei@gmail.com>
2020-07-07 20:54:23 +0800

    Add Proxy Config (Chrome)

--- src/main/java/com/example/app/util/ChromeWebDriverHelper.java
+++ src/main/java/com/example/app/util/ChromeWebDriverHelper.java
@@ -2,8 +2,11 @@ package com.example.app.util;
 
 import java.util.HashMap;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class ChromeWebDriverHelper extends WebDriverHelper {
 	@Override
@@ -21,6 +24,20 @@ public class ChromeWebDriverHelper extends WebDriverHelper {
 				options.addArguments("headless");
 			System.setProperty("webdriver.chrome.driver",
 					PropertiesReader.getProperty("web.driver.path", "src/main/resources/drivers/chromedriver.exe"));
+
+			if (PropertiesReader.getProperty("proxy.enabled", false)) {
+				Proxy proxy = new Proxy();
+				String proxyUrl = PropertiesReader.getProperty("proxy.url", "http://localhost:8081");
+				System.out.println("Proxy through " + proxyUrl);
+				proxy.setSslProxy(proxyUrl);
+				proxy.setHttpProxy(proxyUrl);
+				DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+				capabilities.setCapability("proxy", proxy);
+				capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				options.addArguments("--proxy-bypass-list=<-loopback>");
+				options.merge(capabilities);
+			}
+
 			this.webDriver = new ChromeDriver(options);
 		} catch (Exception e) {
 			e.printStackTrace();
